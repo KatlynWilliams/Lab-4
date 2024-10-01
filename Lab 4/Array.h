0e6164aae6854f6bebfd03a33d6552f80ff2e5ba
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-01 13:55:48 -0500

    Update Array.h

--- Lab 4/Array.h
+++ Lab 4/Array.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
 class Array
 {
 private:
 	int size;
 	int* data;
 public:
-	explicit Array();
+	explicit Array(int size);
 	Array(const Array& source);
 	~Array();
 };
