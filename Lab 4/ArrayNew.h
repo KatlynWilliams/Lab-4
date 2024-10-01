f83e277bf2e5b633f32f387b1b81dba674544868
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-01 14:28:31 -0500

    Update ArrayNew.h

--- Lab 4/ArrayNew.h
+++ Lab 4/ArrayNew.h
@@ -1,5 +1,15 @@
 #pragma once
-class ArrayNew
-{
-};
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class ArrayNew {
+	private:
+		int size;
+		std::unique_ptr<int> data;
+	public:
+		ArrayNew(int size) : {size};
+		ArrayNew(const ArrayNew& source); //copy constructor
+		//smart arrays do not need deconstructors
+}
 
