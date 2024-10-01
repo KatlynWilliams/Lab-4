6061378f2e90a4cdf91ccd1f4dbae8d631f3e284
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-01 15:04:31 -0500

    Update ArrayNew.cpp
    fixed and no main function within

--- Lab 4/ArrayNew.cpp
+++ Lab 4/ArrayNew.cpp
@@ -1,18 +1,16 @@
 #include "ArrayNew.h"
-#include <memory>
 
-ArrayNew::ArrayNew(int size) : {size} {
-	std::cout << "Create an array with Unique Pointer." << std::endl;
-	data = make_unique<int[](size)>;
+ArrayNew::ArrayNew(int size) : size(size), data(std::make_unique<int[]>(size)) { //creates the array with a unique point with given size
+    std::cout << "Created an array with Unique Pointer of size " << size << "." << std::endl;
 }
 
-ArrayNew::ArrayNew(const ArrayNew& source) {
-	std::cout << "Create an array copy." << std::endl;
-	data = make_unique<int[]>(source.data);
+ArrayNew::ArrayNew(const ArrayNew& source) : size(source.size), data(std::make_unique<int[]>(source.size)) { //Goes through and creates a copy of the array with a unique pointer
+    std::cout << "Created an array copy with Unique Pointer." << std::endl;
+    for (int i = 0; i < size; ++i) {
+        data[i] = source.data[i]; // Deep copy
+    }
 }
 
-
-int main() {
-	ArrayNew array1(2);
-	ArrayNew array2(array1);
+ArrayNew::~ArrayNew() { //Deletes all objects
+    std::cout << "Deleted an array with Unique Pointer of size " << size << "." << std::endl;
 }
