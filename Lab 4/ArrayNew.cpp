0e571f82b527d3090c097d920711bd72f7330f40
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-01 14:28:15 -0500

    Update ArrayNew.cpp

--- Lab 4/ArrayNew.cpp
+++ Lab 4/ArrayNew.cpp
@@ -1,1 +1,18 @@
 #include "ArrayNew.h"
+#include <memory>
+
+ArrayNew::ArrayNew(int size) : {size} {
+	std::cout << "Create an array with Unique Pointer." << std::endl;
+	data = make_unique<int[](size)>;
+}
+
+ArrayNew::ArrayNew(const ArrayNew& source) {
+	std::cout << "Create an array copy." << std::endl;
+	data = make_unique<int[]>(source.data);
+}
+
+
+int main() {
+	ArrayNew array1(2);
+	ArrayNew array2(array1);
+}
