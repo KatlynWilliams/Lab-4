ee5d2656e793b28c9eba54d2ef12d88674688a21
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-01 13:55:22 -0500

    Update Array.cpp

--- Lab 4/Array.cpp
+++ Lab 4/Array.cpp
@@ -1,11 +1,26 @@
 #include "Array.h"
+#include <iostream>
+using namespace std;
 
-Array::Array(){
-	
+Array::Array(int size){
+	std::cout << "Create an array." << std::endl;
+	data = new int(size);
 }
-Array::Array(){
-	
+
+Array::Array(const Array& source){
+	std::cout << "Copying array with copy constructor." << std::endl;
+	size = (source.size);
+	data = new int(size);
 }
+
 Array::~Array() {
-	delete[] data;
+	std::cout << "Delete object." << std::endl;
+	for (int i = 0; i < size; i++) {
+		delete[] data;
+	}
+}
+
+int main() {
+	Array array1(2);
+	Array array2(array1);
 }
