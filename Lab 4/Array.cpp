1699df5d64b4e4ad3254e95f7a4d2dee5c05886e
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-01 15:03:59 -0500

    Update Array.cpp
    Without main function within

--- Lab 4/Array.cpp
+++ Lab 4/Array.cpp
@@ -2,25 +2,21 @@
 #include <iostream>
 using namespace std;
 
-Array::Array(int size){
+Array::Array(int size){ //create array with constructor
 	std::cout << "Create an array." << std::endl;
 	data = new int(size);
 }
 
-Array::Array(const Array& source){
+Array::Array(const Array& source) { //creates a copy of the array and assigns it to a new arrau
 	std::cout << "Copying array with copy constructor." << std::endl;
 	size = (source.size);
 	data = new int(size);
 }
 
-Array::~Array() {
+Array::~Array() { //Cycles through and deletes all objects.
 	std::cout << "Delete object." << std::endl;
 	for (int i = 0; i < size; i++) {
 		delete[] data;
 	}
 }
 
-int main() {
-	Array array1(2);
-	Array array2(array1);
-}
