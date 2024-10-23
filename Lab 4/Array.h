dd5a1179917d5f2bbade5fe266469e4ca9fa258b
KatlynWilliams <frogkatlyn@gmail.com>
2024-10-23 00:03:30 -0500

    Update Array.h

--- Lab 4/Array.h
+++ Lab 4/Array.h
@@ -1,23 +1,20 @@
 #pragma once
 #include <iostream>
-using namespace std;
+#include <stdexcept>
 
-class Array
-{
+class Array {
 private:
-	int size;
-	int* data;
+    int size;
+    int* data;
 public:
-	explicit Array(int size);
-	Array(const Array& source);
-	~Array();
-	Array& operator=(const Array& source);
+    explicit Array(int size);
+    Array(const Array& source);
+    ~Array();
+    Array& operator=(const Array& source);
 
-	//Overloaded Subscript Operator
-	int& operator[](std::size_t index) const;
+    // Overloaded Subscript Operator
+    int& operator[](std::size_t index) const;
 
-	//Overloaded Dereference(Indirection) Operator
-	int& operator*() const;
-
+    // Overloaded Dereference (Indirection) Operator
+    int& operator*() const;
 };
-
